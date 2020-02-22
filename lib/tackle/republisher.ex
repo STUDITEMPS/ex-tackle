24d88b0a0e21d46847100a90e9c353f8be83039a
Andreas Riemer <andreas.riemer@gmail.com>
2020-02-22 11:11:42 +0100

    IO.write's raus

--- lib/tackle/republisher.ex
+++ lib/tackle/republisher.ex
@@ -5,26 +5,22 @@ defmodule Tackle.Republisher do
   def republish(rabbitmq_url, dead_queue_name, exchange, routing_key, count) do
     Tackle.execute(rabbitmq_url, fn channel ->
       0..(count - 1)
-      |> Enum.each(fn index ->
-        IO.write("(#{index}) ")
-
+      |> Enum.each(fn _ ->
         republish_one_message(channel, dead_queue_name, exchange, routing_key)
       end)
     end)
   end
 
   defp republish_one_message(channel, dead_queue_name, exchange, routing_key) do
-    IO.write("Fetching message... ")
-
     case AMQP.Basic.get(channel, dead_queue_name) do
       {:empty, _} ->
-        IO.puts("no more messages")
+        Logger.debug("No messages to republish in '#{dead_queue_name}'")
 
       {:ok, message, %{delivery_tag: tag}} ->
         AMQP.Basic.publish(channel, exchange, routing_key, message, persistent: true)
         AMQP.Basic.ack(channel, tag)
 
-        IO.puts("republished")
+        Logger.debug("Republished a message with delivery tag '#{tag}' to '#{dead_queue_name}'")
     end
   end
 end
