ece97e276463e22e72bea48aa61c333d6bd266d8
Eduard Litau <eduard.litau@gmail.com>
2020-05-14 17:24:50 +0200

    Simplified test

--- test/integration/connection_errors_test.exs
+++ test/integration/connection_errors_test.exs
@@ -7,16 +7,15 @@ defmodule Tackle.ConnectionErrorsTest do
   sending any messages.
   """
   use ExUnit.Case
-
   alias Support
   alias Support.MessageTrace
 
-  defmodule AckFailConsumer do
+  defmodule FailConsumer do
     use Tackle.Consumer,
       rabbitmq_url: "amqp://localhost",
-      remote_exchange: "test-exchange",
+      remote_exchange: "ex-tackle.test-exchange",
       routing_key: "test-messages",
-      service: "connection-errors-service",
+      service: "ex-tackle.connection-errors-service",
       connection_id: :ack_fail,
       retry_limit: 0
 
@@ -32,113 +31,30 @@ defmodule Tackle.ConnectionErrorsTest do
     end
   end
 
-  defmodule DelayFailConsumer do
-    use Tackle.Consumer,
-      rabbitmq_url: "amqp://localhost",
-      remote_exchange: "test-exchange",
-      routing_key: "test-messages",
-      service: "connection-errors-service",
-      connection_id: :delay_fail,
-      retry_delay: 1,
-      retry_limit: 3
-
-    def handle_message(message) do
-      message |> MessageTrace.save("connection-errors")
-
-      Tackle.Connection.get_all()
-      |> Keyword.get(:delay_fail)
-      |> Map.get(:pid)
-      |> Process.exit(:kill)
-
-      # exception
-      raise ArgumentError
-    end
-  end
-
-  defmodule DeadFailConsumer do
-    use Tackle.Consumer,
-      rabbitmq_url: "amqp://localhost",
-      remote_exchange: "test-exchange",
-      routing_key: "test-messages",
-      service: "connection-errors-service",
-      connection_id: :dead_fail,
-      retry_limit: 0
-
-    def handle_message(message) do
-      message |> MessageTrace.save("connection-errors")
-
-      Tackle.Connection.get_all()
-      |> Keyword.get(:dead_fail)
-      |> Map.get(:pid)
-      |> Process.exit(:kill)
-
-      # exception
-      raise ArgumentError
-    end
-  end
-
   @publish_options %{
     rabbitmq_url: "amqp://localhost",
-    exchange: "test-exchange",
+    exchange: "ex-tackle.test-exchange",
     routing_key: "test-messages"
   }
-  @timeout 10
+  @timeout 100
 
-  setup do
+  test "connection dies" do
+    # Setup
+    Support.cleanup!(FailConsumer)
     MessageTrace.clear("connection-errors")
-
-    :ok
-  end
-
-  test "connection is lost before ack is sent" do
-    task =
-      Task.async(fn ->
-        {:ok, pid} = AckFailConsumer.start_link()
-        pid
-      end)
-
-    pid = Task.await(task)
-
-    Tackle.publish("Hi!", @publish_options)
-
-    :timer.sleep(@timeout)
-    refute Process.alive?(pid)
-    assert MessageTrace.content("connection-errors") == "Hi!"
-  end
-
-  test "connection is lost while pushing message to delay queue" do
-    task =
-      Task.async(fn ->
-        {:ok, pid} = DelayFailConsumer.start_link()
-        pid
-      end)
-
-    pid = Task.await(task)
-
-    Tackle.publish("Hi!", @publish_options)
-
     :timer.sleep(@timeout)
 
-    assert MessageTrace.content("connection-errors") == "Hi!"
-
-    refute Process.alive?(pid)
-  end
-
-  test "connection is lost while pushing message to dead queue" do
-    task =
-      Task.async(fn ->
-        {:ok, pid} = DeadFailConsumer.start_link()
-        pid
-      end)
-
-    pid = Task.await(task)
-
-    Tackle.publish("Hi!", @publish_options)
-
+    # Start Consumer
+    pid = start_supervised!(FailConsumer)
+    assert Process.alive?(pid)
     :timer.sleep(@timeout)
 
-    assert MessageTrace.content("connection-errors") == "Hi!"
+    # Send message which kills connection
+    Tackle.publish("Hi!", @publish_options)
+    :timer.sleep(@timeout)
 
+    # Assert consumer died
     refute Process.alive?(pid)
+    assert MessageTrace.content("connection-errors") == "Hi!"
   end
 end
